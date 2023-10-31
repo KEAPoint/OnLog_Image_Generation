3ecb8fbe26ead85e250955390db1b06310818fa9
namsh1125 <namsh1125@naver.com>
2023-11-01 02:10:09 +0900

    요청 및 응답 data 수정

--- app/main.py
+++ app/main.py
@@ -1,215 +1,59 @@
-from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
 import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import WordPunctTokenizer
-
-# Google Cloud Translation 라이브러리를 가져옵니다.
 from google.cloud import translate_v2 as translate
+from typing import List
+from pydantic import BaseModel
+
+class ImageRequest(BaseModel):
+    content: str
+    hashtag: List[str]
+
+class ImageData(BaseModel):
+    imageUrl: List[str]
+
+class ImageResponse(BaseModel):
+    isSuccess: bool
+    code: int
+    message: str
+    data: ImageData
 
 app = FastAPI()
 
-# 구글 클라우드 키 경로 설정하기
+# 구글 클라우드 키 경로 설정
 translate_client = translate.Client.from_service_account_json("my-key.json")
 
-@app.post("/generate-images", response_class=HTMLResponse)
-async def generate_image(request: Request):
-    form_data = await request.form()
+@app.post("/generate-images", response_model=ImageResponse)
+async def generate_image(request: ImageRequest):
+    text = request.content
 
-    text = form_data.get("text")
-
-    # Translate the input text to English using Google Cloud Translation API.
     result = translate_client.translate(text, target_language="en")
 
-    # Get the translated text.
     translated_text = result["input"]
 
     tokenizer = WordPunctTokenizer()
 
     stop_words = [
-        ".",
-        ",",
-        '"',
-        "their",
-        "a",
-        "other",
-        "so",
-        "that",
-        "being",
-        "between",
-        "ours",
-        "yourselves",
-        "what",
-        "if",
-        "each",
-        "haven",
-        "you",
-        "they",
-        "not",
-        "because",
-        "against",
-        "mustn't",
-        "them",
-        "over",
-        "you're",
-        "are",
-        "won't",
-        "its",
-        "off",
-        "am",
-        "with",
-        "there",
-        "needn't",
-        "hadn't",
-        "about",
-        "nor",
-        "just",
-        "by",
-        "should",
-        "needn",
-        "shan",
-        "until",
-        "that'll",
-        "more",
-        "you'll",
-        "you've",
-        "will",
-        "mightn",
-        "you'd",
-        "up",
-        "yours",
-        "through",
-        "hasn't",
-        "ma",
-        "my",
-        "such",
-        "itself",
-        "under",
-        "too",
-        "which",
-        "during",
-        "was",
-        "than",
-        "an",
-        "been",
-        "down",
-        "these",
-        "should've",
-        "how",
-        "herself",
-        "the",
-        "here",
-        "to",
-        "having",
-        "don",
-        "t",
-        "theirs",
-        "does",
-        "on",
-        "both",
-        "wouldn't",
-        "don't",
-        "only",
-        "who",
-        "when",
-        "were",
-        "into",
-        "any",
-        "shouldn",
-        "and",
-        "why",
-        "couldn't",
-        "haven't",
-        "or",
-        "where",
-        "she",
-        "me",
-        "him",
-        "at",
-        "below",
-        "his",
-        "her",
-        "then",
-        "did",
-        "this",
-        "he",
-        "it's",
-        "few",
-        "most",
-        "can",
-        "mustn",
-        "very",
-        "s",
-        "himself",
-        "again",
-        "ll",
-        "yourself",
-        "further",
-        "doing",
-        "some",
-        "but",
-        "i",
-        "couldn",
-        "aren't",
-        "it",
-        "now",
-        "ourselves",
-        "re",
-        "have",
-        "after",
-        "our",
-        "your",
-        "out",
-        "wasn't",
-        "mightn't",
-        "themselves",
-        "whom",
-        "once",
-        "is",
-        "hadn",
-        "y",
-        "doesn't",
-        "weren",
-        "we",
-        "before",
-        "as",
-        "o",
-        "own",
-        "above",
-        "wouldn",
-        "of",
-        "all",
-        "shan't",
-        "be",
-        "weren't",
-        "while",
-        "d",
-        "from",
-        "aren",
-        "isn",
-        "won",
-        "didn",
-        "no",
-        "hasn",
-        "those",
-        "didn't",
-        "isn't",
-        "she's",
-        "ain",
-        "same",
-        "shouldn't",
-        "do",
-        "m",
-        "for",
-        "hers",
-        "had",
-        "doesn",
-        "myself",
-        "in",
-        "wasn",
-        "has",
-        "ve",
-    ]  # Exclude articles and punctuation
+        ".", ",", '"', "their", "a", "other", "so", "that", "being", "between", "ours", 
+        "yourselves", "what", "if", "each", "haven", "you", "they", "not", "because", "against", 
+        "mustn't", "them", "over", "you're", "are", "won't", "its", "off", "am", "with", "there", 
+        "needn't", "hadn't", "about", "nor", "just", "by", "should", "needn", "shan", "until", 
+        "that'll", "more", "you'll", "you've", "will", "mightn", "you'd", "up", "yours", 
+        "through", "hasn't", "ma", "my", "such", "itself", "under", "too", "which", "during", 
+        "was", "than", "an", "been", "down", "these", "should've", "how", "herself", "the", 
+        "here", "to", "having", "don", "t", "theirs", "does", "on", "both", "wouldn't", "don't", 
+        "only", "who", "when", "were", "into", "any", "shouldn", "and", "why", "couldn't", 
+        "haven't", "or", "where", "she", "me", "him", "at", "below", "his", "her", "then", 
+        "did", "this", "he", "it's", "few", "most", "can", "mustn", "very", "s", "himself", 
+        "again", "ll", "yourself", "further", "doing", "some", "but", "i", "couldn", "aren't", 
+        "it", "now", "ourselves", "re", "have", "after", "our", "your", "out", "wasn't", 
+        "mightn't", "themselves", "whom", "once", "is", "hadn", "y", "doesn't", "weren", "we", 
+        "before", "as", "o", "own", "above", "wouldn", "of", "all", "shan't", "be", "weren't", 
+        "while", "d", "from", "aren", "isn", "won", "didn", "no", "hasn", "those", "didn't", 
+        "isn't", "she's", "ain", "same", "shouldn't", "do", "m", "for", "hers", "had", "doesn", 
+        "myself", "in", "wasn", "has", "ve",
+    ]
 
     vectorizer = TfidfVectorizer(tokenizer=tokenizer.tokenize, stop_words=stop_words)
 
@@ -217,7 +61,6 @@ async def generate_image(request: Request):
 
     feature_names = vectorizer.get_feature_names_out()
 
-    # Sort by tf-idf score and get top 5 keywords
     sorted_keywords_indices = tfidf_matrix.toarray().argsort()[0][::-1][:5]
 
     keywords_prompt = " ".join(
@@ -242,19 +85,20 @@ async def generate_image(request: Request):
             raise HTTPException()
 
         response_json = r.json()
-    
+
         image_url = response_json["images"][0]["image"]
 
-        return {
-            'isSuccess': True,
-            'code': 200,
-            'message': '요청에 성공하였습니다.',
-            'data': image_url,
-        }
+        return ImageResponse(
+            isSuccess=True,
+            code=200,
+            message='요청에 성공하였습니다.',
+            data=ImageData(imageUrl=[image_url]),
+        )
 
     except HTTPException as e:
-        return {
-        'isSuccess': False,
-        'code': 400,
-        'message': '이미지 생성에 실패하였습니다.'
-    }
+        return ImageResponse(
+            isSuccess=False,
+            code=400,
+            message='이미지 생성에 실패하였습니다.',
+            data=ImageData(imageUrl=[]),
+        )
